c72808063f0d99dff254a39d7bc7faf32ac84122
dottenad <ottenadd@gmail.com>
2021-12-20 09:49:26 -0800

    add

--- myApp.js
+++ myApp.js
@@ -8,16 +8,6 @@ app.get("/", (req, res) => {
 app.use("/public", express.static(__dirname + "/public"));
 
 
-if (process.env.MESSAGE_STYLE=="uppercase"){
-  app.get("/json", function(req, res) {
-    return res.json({ "message": "HELLO JSON" })
-  });
-} 
-else {
-  app.get("/json", function(req, res) {
-    return res.json({ "message": "Hello json" })
-  });
-}
 
 
 
