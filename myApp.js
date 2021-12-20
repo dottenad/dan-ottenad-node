46dea028ec8b1a52542d71a9246bff8277e8dd71
Dan Ottenad <dan@drawbridgemarketing.com>
2021-12-19 16:38:09 -0800

    add

---
+++ myApp.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello Express");
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ module.exports = app;
